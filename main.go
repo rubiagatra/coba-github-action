69fb5baa8de4dcacbdba34529263fdb4bc7dd838
Doni Rubiagatra <doni@zero-one-group.com>
2021-09-25 12:07:06 +0700

    Add heroku support

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/rubiagatra/coba-github-action/calc"
 )
 
 func main() {
-	fmt.Println(calc.Add(3, 4))
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(fmt.Sprintf("Hello, World 👋! Add(3 + 3 = %d)", calc.Add(3, 3)))
+	})
+
+	app.Listen(":3000")
 }
